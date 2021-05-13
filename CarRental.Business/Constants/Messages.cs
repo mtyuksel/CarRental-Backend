341925bc8725b4fc5ed3ebaba1c037ec439cf9ae
mtyuksel <yukselmtevfik@gmail.com>
2021-05-13 21:05:35 +0300

    Messages, regions added.

--- CarRental.Business/Constants/Messages.cs
+++ CarRental.Business/Constants/Messages.cs
@@ -7,10 +7,18 @@ namespace CarRental.Business.Constants
 {
     public static class Messages
     {
-        public static string CarAdded = "Car added!";
+        #region Error Messages
         public static string CarPriceNotValid = "Car price is not valid!";
         public static string CarNameNotValid = "Car name is not valid!";
         public static string CarNameAndPriceNotValid = "Car name and price are not valid!";
+        public static string CarHasNotYetBeenReturned = "The car has not yet been returned!";
+        #endregion
+
+        #region Success Messages
+        public static string SuccesfullyUpdated = "Successfully updated.";
+        public static string SuccesfullyAdded = "Successfully added.";
+        #endregion
+
         public static string MaintenanceTime = "The service is under maintenance. Please try again in 1 hour.";
     }
 }
