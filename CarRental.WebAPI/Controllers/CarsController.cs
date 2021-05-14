a0d5521fd83384dcb11318575781591d81ba5fc1
mtyuksel <yukselmtevfik@gmail.com>
2021-05-14 21:40:03 +0300

    Unused libraries removed.

--- CarRental.WebAPI/Controllers/CarsController.cs
+++ CarRental.WebAPI/Controllers/CarsController.cs
@@ -1,12 +1,6 @@
 ﻿using CarRental.Business.Abstract;
-using CarRental.Core.Utilities.Results;
 using CarRental.Entity.Concrete;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CarRental.WebAPI.Controllers
 {
--- CarRental.WebAPI/Controllers/ColorsController.cs
+++ CarRental.WebAPI/Controllers/ColorsController.cs
@@ -1,11 +1,6 @@
 ﻿using CarRental.Business.Abstract;
 using CarRental.Entity.Concrete;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CarRental.WebAPI.Controllers
 {
