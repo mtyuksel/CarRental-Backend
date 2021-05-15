8227b4a885bc3d49b2a3cae0305c5376a9af5332
mtyuksel <yukselmtevfik@gmail.com>
2021-05-15 15:59:40 +0300

    Unused libs. removed.

--- CarRental.WebAPI/Controllers/CarImagesController.cs
+++ CarRental.WebAPI/Controllers/CarImagesController.cs
@@ -8,8 +8,6 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CarRental.WebAPI.Controllers
 {
