fdfd6afdeb78682af7607355be354bf70d85b446
mtyuksel <yukselmtevfik@gmail.com>
2021-06-05 00:23:36 +0300

    Classes added for central error handling.

---
+++ CarRental.Core/Entity/Concrete/ErrorDetails.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace CarRental.Core.Entity.Concrete
+{
+    public class ErrorDetails
+    {
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ CarRental.Core/Entity/Concrete/ValidationErrorDetails.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation.Results;
+using System.Collections.Generic;
+
+namespace CarRental.Core.Entity.Concrete
+{
+    class ValidationErrorDetails : ErrorDetails
+    {
+        public IEnumerable<ValidationFailure> Errors { get; set; }
+    }
+}
