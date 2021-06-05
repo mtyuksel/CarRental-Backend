71b5f6fcb27e9b9107ee22c24e4f85d09b821ddc
mtyuksel <yukselmtevfik@gmail.com>
2021-06-05 10:42:34 +0300

    API base URL dynamically added to image path.

--- CarRental.WebAPI/Controllers/CarsController.cs
+++ CarRental.WebAPI/Controllers/CarsController.cs
@@ -2,20 +2,21 @@
 using CarRental.Entity.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System;
 
 namespace CarRental.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CarsController : GenericBaseController<Car, ICarService> 
+    public class CarsController : GenericBaseController<Car, ICarService>
     {
         private string _uploadPath;
         private string _defaultImageFullPath;
-        
+
 
         public CarsController(ICarService carService, IConfiguration configuration) : base(carService)
         {
-            this._uploadPath =  configuration.GetSection("DefaultOptions").GetSection("Image").GetSection("UploadPath").Value;
+            this._uploadPath = configuration.GetSection("DefaultOptions").GetSection("Image").GetSection("UploadPath").Value;
 
             string defaultImagePath = configuration.GetSection("DefaultOptions").GetSection("Image").GetSection("DefaultUploadPath").Value;
             string defaultImageName = configuration.GetSection("DefaultOptions").GetSection("Image").GetSection("DefaultName").Value;
@@ -26,14 +27,16 @@ namespace CarRental.WebAPI.Controllers
         [HttpGet("getcardetails")]
         public IActionResult GetCarDetails()
         {
-            return base.GetResponseByResultSuccess(base._tService.GetCarDetails(_uploadPath, _defaultImageFullPath));
+            string baseUrl = $"{this.Request.Scheme}://{this.Request.Host}";
+            return base.GetResponseByResultSuccess(base._tService.GetCarDetails(baseUrl + _uploadPath, baseUrl + _defaultImageFullPath));
         }
 
         [HttpGet("getbylocation")]
         public IActionResult GetByLocation(int locationID)
         {
-            return base.GetResponseByResultSuccess(base._tService.GetDetailedCarsByLocation(locationID, _uploadPath, _defaultImageFullPath));
+            string baseUrl = $"{this.Request.Scheme}://{this.Request.Host}";
+            return base.GetResponseByResultSuccess(base._tService.GetDetailedCarsByLocation(locationID, baseUrl + _uploadPath, baseUrl + _defaultImageFullPath));
         }
 
     }
-} 
+}
